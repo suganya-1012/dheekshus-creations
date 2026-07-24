70589be965dc2f419bfab783557302d53e697d61
suganya-1012 <sugs86@gmail.com>
2026-07-24 22:38:41 +0530

    Added utility functions

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,1 +1,34 @@
+/*
+=========================================
+Dheekshu's Creations
+Utility Functions
+=========================================
+*/
 
+// Read cart from browser
+function getCart() {
+    return JSON.parse(localStorage.getItem("cart")) || [];
+}
+
+// Save cart
+function saveCart(cart) {
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+// Update cart badge
+function updateCartCount() {
+
+    const cart = getCart();
+
+    let total = 0;
+
+    cart.forEach(item => {
+        total += item.quantity;
+    });
+
+    const badge = document.getElementById("cart-count");
+
+    if (badge) {
+        badge.innerText = total;
+    }
+}
